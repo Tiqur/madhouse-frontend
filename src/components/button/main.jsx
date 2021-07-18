a564757acc5a9eae86a638b3abb4a7cd1678e8de
Tiqur <Tiqur7@gmail.com>
2021-07-18 13:07:35 -0700

    Button component

---
+++ src/components/button/main.jsx
@@ -0,0 +1,20 @@
+import styles from './styles.module.scss';
+import { Text } from '../index.js';
+
+const Button = (props) => {
+  return (
+    <div className={styles.button} style={{
+      ...{
+        width: props.width,
+        height: props.height,
+        background: props.primary ? '#470000' : '',
+        border: "2px solid #A10000"
+
+        },
+      ...props.style}}>
+      <Text size={1.8} weight='bold'>{props.text}</Text>
+    </div>
+  )
+}
+
+export default Button;
--- src/pages/home/main.jsx
+++ src/pages/home/main.jsx
@@ -1,5 +1,5 @@
 import styles from './styles.module.scss';
-import { Text } from '../../components/index.js';
+import { Text, Button } from '../../components/index.js';
 
 const HomePage = () => {
   return (
@@ -9,6 +9,7 @@ const HomePage = () => {
           <Text size={4}>Welcome to the Madhouse!</Text>
           <Text size={2} color="#CDCDCD">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras augue arcu, luctus in massa quis, efficitur faucibus magna. Vivamus volutpat lacus at elit tempor faucibus.   Phasellus iaculis consequat libero, a malesuada nulla viverra!</Text>
         </div>
+        <Button primary style={{marginTop: '20px'}} text="Join the community" width='11em' height='4em'/>
       </div>
     </div>
   )
