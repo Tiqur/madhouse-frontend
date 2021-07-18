7357a9a104d9c82a4d9d2d4de75811bde4679fb4
Tiqur <Tiqur7@gmail.com>
2021-07-18 10:26:59 -0700

    Setup hero

--- src/pages/home/main.jsx
+++ src/pages/home/main.jsx
@@ -1,6 +1,16 @@
+import styles from './styles.module.scss';
+import { Text } from '../../components/index.js';
+
 const HomePage = () => {
   return (
-    <p/>
+    <div className={styles.main_container}>
+      <div className={styles.content, styles.hero}>
+        <div className={styles.text_container}>
+          <Text size={4}>Welcome to the Madhouse!</Text>
+          <Text size={2} color="#CDCDCD">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras augue arcu, luctus in massa quis, efficitur faucibus magna. Vivamus volutpat lacus at elit tempor faucibus.   Phasellus iaculis consequat libero, a malesuada nulla viverra!</Text>
+        </div>
+      </div>
+    </div>
   )
 }
 
