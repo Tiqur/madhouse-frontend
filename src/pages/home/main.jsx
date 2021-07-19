90b2a76da684cc43ab2cd8b09114b395b0e16f74
Tiqur <Tiqur7@gmail.com>
2021-07-19 12:30:49 -0700

    Responsive hero and video placeholder

--- src/pages/home/main.jsx
+++ src/pages/home/main.jsx
@@ -5,11 +5,16 @@ const HomePage = () => {
   return (
     <div className={styles.main_container}>
       <div className={styles.content, styles.hero}>
-        <div className={styles.text_container}>
-          <Text size={5}>Welcome to the Madhouse!</Text>
-          <Text margin='1em 0 0 0' size={2} color="#CDCDCD">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras augue arcu, luctus in massa quis, efficitur faucibus magna. Vivamus volutpat lacus at elit tempor faucibus.   Phasellus iaculis consequat libero, a malesuada nulla viverra in</Text>
+        <div className={styles.hero_info}>
+          <div className={styles.text_container}>
+            <Text size={5}>Welcome to the Madhouse!</Text>
+            <Text margin='1em 0 0 0' size={2} color="#CDCDCD">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras augue arcu, luctus in massa quis, efficitur faucibus magna. Vivamus volutpat lacus at elit tempor faucibus.   Phasellus iaculis consequat libero, a malesuada nulla viverra in</Text>
+          </div>
+          <Button primary style={{marginTop: '2em'}} text="Join the community" width='11em' height='4em'/>
         </div>
-        <Button primary style={{marginTop: '2em'}} text="Join the community" width='11em' height='4em'/>
+        <div className={styles.hero_content}>
+          <div className={styles.video_placeholder}/>
+        </div>
       </div>
     </div>
   )
