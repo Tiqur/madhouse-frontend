5317d958239f8c89f82493c498deaafbd8d85107
Tiqur <Tiqur7@gmail.com>
2021-07-20 14:03:22 -0700

    Setup Footer component

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,5 @@ export {default as Layout} from './layout/main.jsx';
 export {default as Navbar} from './navbar/main.jsx';
 export {default as Text} from './text/main.jsx';
 export {default as Button} from './button/main.jsx';
+export {default as Footer} from './footer/main.jsx';
 
--- src/pages/home/main.jsx
+++ src/pages/home/main.jsx
@@ -1,23 +1,26 @@
 import styles from './styles.module.scss';
-import { Text, Button } from '../../components/index.js';
+import { Text, Button, Footer } from '../../components/index.js';
 
 const HomePage = () => {
   return (
-    <div className={styles.main_container}>
-      <div className={styles.content, styles.hero}>
-        <div className={styles.hero_info}>
-          <div className={styles.text_container}>
-            <Text size={5}>Welcome to the Madhouse!</Text>
-            <Text margin='1em 0 0 0' size={2} color="#CDCDCD">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras augue arcu, luctus in massa quis, efficitur faucibus magna. Vivamus volutpat lacus at elit tempor faucibus.   Phasellus iaculis consequat libero, a malesuada nulla viverra in</Text>
+    <>
+      <div className={styles.main_container}>
+        <div className={styles.content, styles.hero}>
+          <div className={styles.hero_info}>
+            <div className={styles.text_container}>
+              <Text size={5}>Welcome to the Madhouse!</Text>
+              <Text margin='1em 0 0 0' size={2} color="#CDCDCD">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras augue arcu, luctus in massa quis, efficitur faucibus magna. Vivamus volutpat lacus at elit tempor faucibus.   Phasellus iaculis consequat libero, a malesuada nulla viverra in</Text>
+            </div>
+            <Button primary style={{marginTop: '2em'}} text="Join the community" width='11em' height='4em'/>
           </div>
-          <Button primary style={{marginTop: '2em'}} text="Join the community" width='11em' height='4em'/>
-        </div>
-        <div className={styles.hero_content}>
-          <Text margin='0 0 0.5em 0' size={1.5}>Latest Video:</Text>
-          <iframe width="512" height="288" src="https://www.youtube.com/embed/fylt8A_oL60" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <div className={styles.hero_content}>
+            <Text margin='0 0 0.5em 0' size={1.5}>Latest Video:</Text>
+            <iframe width="512" height="288" src="https://www.youtube.com/embed/fylt8A_oL60" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
         </div>
       </div>
-    </div>
+      <Footer/>
+    </>
   )
 }
 
