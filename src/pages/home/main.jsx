772dc31c0e606239ec1d78083f56c8addf829ff9
Tiqur <Tiqur7@gmail.com>
2021-07-22 22:04:09 -0700

    Replace video with logo on home page

--- src/pages/home/main.jsx
+++ src/pages/home/main.jsx
@@ -1,5 +1,6 @@
 import styles from './styles.module.scss';
 import { Text, Button, Footer } from '../../components/index.js';
+import { LogoPng } from '../../assets/index.js';
 
 const HomePage = () => {
   return (
@@ -13,8 +14,9 @@ const HomePage = () => {
           <Button primary style={{marginTop: '2em'}} text="Join the community" width='11em' height='4em'/>
         </div>
         <div className={styles.hero_content}>
-          <Text margin='0 0 0.5em 0' size={1.5}>Latest Video:</Text>
-          <iframe width="512" height="288" src="https://www.youtube.com/embed/fylt8A_oL60" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          { /* <Text margin='0 0 0.5em 0' size={1.5}>Latest Video:</Text>
+            <iframe width="512" height="288" src="https://www.youtube.com/embed/fylt8A_oL60" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */ }
+          <img src={LogoPng} className={styles.logo} alt='logo'/>
         </div>
       </div>
     </div>
